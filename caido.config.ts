b21b5ec43d58722b5990fae920b6917f3757c451
ChrisCZ <138528727+ChrisCZ2@users.noreply.github.com>
2026-03-11 17:30:55 -0400

    Update caido.config.ts

--- caido.config.ts
+++ caido.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   id,
   name: "Repo Explorer",
   description: "Browse and explore content from GitHub repositories like PayloadsAllTheThings",
-  version: "1.0.3",
+  version: "1.0.5",
   author: {
     name: "Chriscz",
     email: "chrisczv@proton.me",
